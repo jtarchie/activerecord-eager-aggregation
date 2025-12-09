07c4dd2e5102925eac4815060f6369e1fb41a6dc
JT Archie <jt@backerkit.com>
2025-12-09 09:03:18 -0700

    init

---
+++ lib/activerecord/eager/aggregation.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "aggregation/version"
+
+module Activerecord
+  module Eager
+    module Aggregation
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/activerecord/eager/aggregation_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Activerecord::Eager::Aggregation do
+  it "has a version number" do
+    expect(Activerecord::Eager::Aggregation::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
